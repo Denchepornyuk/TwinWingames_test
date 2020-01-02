8d32659a7296907218901619013f43d472484160
Denys <denys.cheporniuk@gmail.com>
2020-01-02 21:38:27 +0200

    Updates

---
+++ precache-manifest.b1d26ce3d4bc22081daf3e1334208b75.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61eecd0bdb534469998295f12bbd6030",
+    "url": "/TwinWingames_test/index.html"
+  },
+  {
+    "revision": "0147083ca495308c6479",
+    "url": "/TwinWingames_test/static/css/main.c2f10969.chunk.css"
+  },
+  {
+    "revision": "0bace40ec1772a0ab28b",
+    "url": "/TwinWingames_test/static/js/2.64a01280.chunk.js"
+  },
+  {
+    "revision": "0147083ca495308c6479",
+    "url": "/TwinWingames_test/static/js/main.815a0d6b.chunk.js"
+  },
+  {
+    "revision": "433c1b50335a57f1cb0e",
+    "url": "/TwinWingames_test/static/js/runtime~main.f1832b32.js"
+  }
+]);
